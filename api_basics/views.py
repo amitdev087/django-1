11364ac21bdd50e4bb5af4bc3be9fd1cf6522980
gopheramit <amitwpu@gmail.com>
2022-09-04 22:17:25 +0530

    REST Framework Function Based API Views

--- api_basics/views.py
+++ api_basics/views.py
@@ -1,3 +1,4 @@
+from re import A
 from django.shortcuts import render
 
 from django.http.response import HttpResponse,JsonResponse 
@@ -5,8 +6,10 @@ from rest_framework.parsers import JSONParser
 from .models import Article
 from .serilizers import ArticleSerializer
 
-# Create your views here.
+from django.views.decorators.csrf import csrf_exempt
 
+# Create your views here.
+@csrf_exempt
 def article_list(request):
     if request.method=="GET":
         articles=Article.objects.all()
@@ -22,4 +25,28 @@ def article_list(request):
             return JsonResponse(serilizer.data ,status=201)
         return JsonResponse(serilizer.errors,status=400)
 
+
+@csrf_exempt
+def article_details(request,pk):
+    try:
+        article=Article.objects.get(pk=pk)
+
+    except Article.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method=="GET":
+        serilizer=ArticleSerializer(article)
+        return JsonResponse(serilizer.data ,status=201)
+
+    elif request.method=="PUT":
+        data=JSONParser().parse(request)
+        serilizer=ArticleSerializer(article,data=data)
+        if serilizer.is_valid():
+            serilizer.save()
+            return JsonResponse(serilizer.data )
+        return JsonResponse(serilizer.errors,status=400)
+
+    elif request.method=="DELETE":
+        article.delete()
+        return HttpResponse(status=204)
 
